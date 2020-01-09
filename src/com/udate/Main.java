308b1cf2422a24dc5aada48f474adae4826d0ed7
Janis Karlsson <janis_k@hotmail.com>
2020-01-09 17:40:33 +0100

    First commit to feature branch FS (File system) Two new packages: FS Udate (actual project using the file system)
    Each of these directories have a file each, jsuts to see that the uploading works well from within iJ.
    
    Add a comment in the Main.java

--- src/com/udate/Main.java
+++ src/com/udate/Main.java
@@ -1,5 +1,10 @@
 package com.udate;
 
+/*
+*  Main file for testing the DB while developing it.
+*
+* */
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/com/udate/fs/Database.java
@@ -0,0 +1,10 @@
+package com.udate.fs;
+
+/*
+Programmerat av Jan-Erik "Janis" Karlsson 2020-01-09
+Programmering i Java EMMJUH19, EC-Utbildning
+CopyLeft 2020 - JanInc
+*/
+
+public class Database {
+}
